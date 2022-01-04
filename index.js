8293bde3208ec912444b0344360a700be7abe237
dsjacob20 <90866861+dsjacob20@users.noreply.github.com>
2022-01-03 18:04:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function j1(){
+open(href="https://en.wikipedia.org/wiki/The_Avengers_(2012_film)");
+}
+    
+function j2(){
+open(href="https://en.wikipedia.org/wiki/Avengers:_Age_of_Ultron");
+}
+    
+function j3(){
+open(href="https://en.wikipedia.org/wiki/Avengers:_Infinity_War");
+}
+function j4(){
+open(href="https://en.wikipedia.org/wiki/Avengers:_Endgame");
+}
+    
+function j5(){
+open(href="https://www.inverse.com/entertainment/avengers-5-release-date-leak-2024-eternals-2");
+}
+    
+    
